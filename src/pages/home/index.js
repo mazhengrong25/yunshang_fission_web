5677d945427ca31cf3c756bd19b0f9c12bdd45c2
wishQAQ <wishtime1024@gmail.com>
2021-02-19 09:29:46 +0800

    flight list

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,7 +2,7 @@
  * @Description: 主页
  * @Author: wish.WuJunLong
  * @Date: 2021-01-11 15:08:05
- * @LastEditTime: 2021-02-05 18:35:09
+ * @LastEditTime: 2021-02-18 12:02:36
  * @LastEditors: wish.WuJunLong
  */
 import React, { Component } from "react";
@@ -21,20 +21,24 @@ export default class index extends Component {
     };
   }
 
-  componentDidMount(){
-    this.getNoticeList()
+  componentDidMount() {
+    this.getNoticeList();
   }
 
   // 获取公告列表
-  getNoticeList(){
-    this.$axios.get('/api/notice')
-      .then(res =>{
-        if(res.errorcode === 10000){
-          this.setState({
-            notice: res.data.data
-          })
-        }
-      })
+  getNoticeList() {
+    this.$axios.get("/api/notice").then((res) => {
+      if (res.errorcode === 10000) {
+        this.setState({
+          notice: res.data.data,
+        });
+      }
+    });
+  }
+
+  // 打开公告详情
+  jumpNoticeDetail(val) {
+    console.log(val);
   }
 
   render() {
@@ -45,14 +49,44 @@ export default class index extends Component {
           <div className="home__banner__background"></div>
           <div className="home__banner__main">
             <div className="home__banner__main__ticketBox">
-              <FlightSearch history={this.props.history}/>
+              <FlightSearch history={this.props.history} />
             </div>
             <div className="home__banner__main__toolBox">
+              <div className="home__banner__main__toolBox__notice">
+                <div className="title">公告通知</div>
+                <div className="notice_main">
+                  {this.state.notice.map((item, index) => {
+                    if (index < 3) {
+                      return (
+                        <div
+                          className="notice_list"
+                          key={index}
+                          onClick={() => this.jumpNoticeDetail(item)}
+                        >
+                          <div className="list_message">
+                            <div className="notice_title">
+                              {item.title}
+                              {index === 0? (
+                                <div className="newNotice">NEW</div>
+                              ): ''}  
+                            </div>
+                            <div className="notice_date">
+                              {this.$moment(item.created_at).format("YYYY-MM-DD")}
+                            </div>
+                          </div>
+                          <div className="list_content">
+                            {item.content.replace(/<[^>]+>/g, "").replace(/&nbsp;/gi, "")}
+                          </div>
+                        </div>
+                      );
+                    }
+                  })}
+                  <div className="moreNotice">查看更多</div>
+                </div>
+              </div>
               <div className="home__banner__main__toolBox__weatherCalendar"></div>
-              <div className="home__banner__main__toolBox__notice"></div>
             </div>
           </div>
-
         </div>
       </div>
     );
